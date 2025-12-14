6973f1798bf7816ce3c0ac5541e843ff707cb94b
Yuri Melo <yurifullstack@gmail.com>
2025-12-14 12:45:31 -0300

    Refactor Author class to inherit from Entity; add optional UUID parameter to constructor and clean up code formatting.

--- src/domain/author.py
+++ src/domain/author.py
@@ -1,7 +1,12 @@
-class Author:
-    def __init__(self, name: str):
+from typing import Optional
+from uuid import UUID
+from src.domain.entity import Entity
+
+class Author(Entity):
+    def __init__(self, name: str, id: Optional[UUID] = None):
+        super().__init__(id)
         self.name = name
         self.books = []
 
     def add_book(self, book):
-        self.books.append(book);
+        self.books.append(book)
